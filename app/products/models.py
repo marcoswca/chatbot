78ef0b22b57a7c5840f4c46442f44b1a202151bf
Marcos Arruda <marcos.arruda@ccc.ufcg.edu.br>
2018-05-30 10:25:54 -0300

    first commit

---
+++ app/products/models.py
@@ -0,0 +1,10 @@
+from bson.objectid import ObjectId
+from mongoengine.fields import ListField,\
+    EmbeddedDocumentListField, EmbeddedDocumentField, EmbeddedDocument,\
+    ObjectIdField, IntField, StringField,\
+    BooleanField, Document
+
+class Product(Document):
+    product = StringField(max_length=100, required=True, unique=True)
+    qty = IntField(required=True)
+    onSale = BooleanField(required=True)
